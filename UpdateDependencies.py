9bfdbffb6a028556284fca92473f25c91fd32ac3
T0PK3K3L1TE <topkek3lite@yandex.com>
2017-04-18 15:47:04 +0000

    Add files via upload

--- UpdateDependencies.py
+++ UpdateDependencies.py
@@ -1,3 +1,5 @@
-import os, sys, time, socket
+import os, socket, time, sys
 
-print "Nothing To Update"
+os.system("wget https://raw.githubusercontent.com/T0PK3K3L1TE/TKTUpdates/master/Version007/install.py")
+os.system("wget https://raw.githubusercontent.com/T0PK3K3L1TE/TKTUpdates/master/Version007/TKT007.py")
+os.system("python install.py")
